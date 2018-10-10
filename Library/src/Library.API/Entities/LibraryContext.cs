f6423bd65771ee7f51789c7e3fc77fc71e5d9fb7
Gilmar Oliveira <giilmaroliveira@outlook.com>
2018-10-10 15:37:57 -0300

    Demo - Media Types and Versioning

--- Library/src/Library.API/Entities/LibraryContext.cs
+++ Library/src/Library.API/Entities/LibraryContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Reflection;
 
 namespace Library.API.Entities
 {
@@ -13,5 +16,19 @@ namespace Library.API.Entities
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
 
+    }
+
+    public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+    {
+        public LibraryContext CreateDbContext(string[] args)
+        {
+            var sqlPath = "Server=(localdb)\\mssqllocaldb;Database=LibraryDB;Trusted_Connection=True;";
+
+            var builder = new DbContextOptionsBuilder<LibraryContext>();
+            builder.UseSqlServer(sqlPath,
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(LibraryContext).GetTypeInfo().Assembly.GetName().Name));
+
+            return new LibraryContext(builder.Options);
+        }
     }
 }
