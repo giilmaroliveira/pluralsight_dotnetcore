1e4da1576d79b47cf818f48ae8992c4b26daf81c
Gilmar Oliveira <giilmaroliveira@outlook.com>
2018-10-10 14:25:29 -0300

    Demo - Working Towards Self-discoverability with a Root Document

--- Library/src/Library.API/Controllers/AuthorsController.cs
+++ Library/src/Library.API/Controllers/AuthorsController.cs
@@ -186,7 +186,7 @@ namespace Library.API.Controllers
             return Ok(linkedResourceToReturn);
         }
 
-        [HttpPost]
+        [HttpPost(Name = "CreateAuthor")]
         public IActionResult CreateAuthor([FromBody] AuthorForCreationDto author)
         {
             if (author == null)
